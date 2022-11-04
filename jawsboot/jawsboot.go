94870700c85adb10f0b17b7eca6a7257e4a23507
Johan Lindh <johan@linkdata.se>
2022-11-04 12:18:30 +0100

    add extra argument to jawsboot

--- jawsboot/jawsboot.go
+++ jawsboot/jawsboot.go
@@ -5,15 +5,14 @@ import "github.com/linkdata/jaws"
 const DefaultBootstrapVersion = "5.2.2"
 const DefaultBootstrapCDN = "https://cdn.jsdelivr.net/npm/bootstrap"
 
-// SetupUsing adds URL's for the given Bootstrap version and CDN to the Jaws.ExtraJS and Jaws.ExtraCSS arrays.
-func SetupUsing(jw *jaws.Jaws, version, cdn string) error {
-	return jw.GenerateHeadHTML(
+// SetupUsing adds URL's for the given Bootstrap version and CDN along with `extra` and calls Jaws.GenerateHeadHTML.
+func SetupUsing(jw *jaws.Jaws, version, cdn string, extra ...string) error {
+	return jw.GenerateHeadHTML(append(extra,
 		cdn+"@"+version+"/dist/js/bootstrap.bundle.min.js",
-		cdn+"@"+version+"/dist/css/bootstrap.min.css",
-	)
+		cdn+"@"+version+"/dist/css/bootstrap.min.css")...)
 }
 
-// Setup calls SetupUsing with DefaultBootstrapVersion and DefaultBootstrapCDN.
-func Setup(jw *jaws.Jaws) error {
-	return SetupUsing(jw, DefaultBootstrapVersion, DefaultBootstrapCDN)
+// Setup calls SetupUsing with DefaultBootstrapVersion and DefaultBootstrapCDN and `extra`.
+func Setup(jw *jaws.Jaws, extra ...string) error {
+	return SetupUsing(jw, DefaultBootstrapVersion, DefaultBootstrapCDN, extra...)
 }
