1627815c1f80bfab54b5005c2901276014cbffa2
Johan Lindh <johan@linkdata.se>
2023-05-02 11:47:40 +0200

    semicolons

--- jaws.js
+++ jaws.js
@@ -164,8 +164,8 @@ function jawsUnloading() {
 	if (jaws instanceof WebSocket) {
 		jaws.removeEventListener('close', jawsFailed);
 		jaws.removeEventListener('error', jawsFailed);
-		jaws.close()
-		jaws = null
+		jaws.close();
+		jaws = null;
 	}
 }
 
