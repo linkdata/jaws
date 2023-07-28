09f9e0be505a7bd244bcbb34a97f3eee3a681a08
Johan Lindh <johan@linkdata.se>
2023-07-28 14:45:32 +0200

    wip

--- wsmsg.go
+++ wsmsg.go
@@ -25,10 +25,10 @@ func (m *wsMsg) Append(b []byte) []byte {
 	b = strconv.AppendInt(b, int64(m.jid), 10)
 	b = append(b, '\n')
 	if m.What != 0 {
-		b = append(b, []byte(m.What.String())...)
+		b = append(b, m.What.String()...)
 	}
 	b = append(b, '\n')
-	b = append(b, []byte(m.Data)...)
+	b = append(b, m.Data...)
 	return b
 }
 
