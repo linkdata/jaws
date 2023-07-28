c6e3d34b73fa881d65ab661d300f464ba0d8c5ed
Johan Lindh <johan@linkdata.se>
2023-07-28 14:48:13 +0200

    wip

--- ws.go
+++ ws.go
@@ -62,7 +62,7 @@ func wsParse(txt []byte) (wsMsg, bool) {
 	if nl1 := bytes.IndexByte(txt, '\n'); nl1 > 0 {
 		if nl2 := bytes.IndexByte(txt[nl1+1:], '\n'); nl2 >= 0 {
 			nl2 += nl1 + 1
-			if jid, err := strconv.ParseInt(string(txt[0:nl1]), 16, 0); err == nil {
+			if jid, err := strconv.Atoi(string(txt[0:nl1])); err == nil {
 				return wsMsg{
 					jid:  int(jid),
 					What: what.Parse(string(txt[nl1+1 : nl2])),
