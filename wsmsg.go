d5dbcdabb8e06b81823091dcb32c196e1715c34c
Johan Lindh <johan@linkdata.se>
2023-07-28 14:44:44 +0200

    wip

--- wsmsg.go
+++ wsmsg.go
@@ -22,7 +22,7 @@ func (m *wsMsg) IsValid() bool {
 }
 
 func (m *wsMsg) Append(b []byte) []byte {
-	b = strconv.AppendInt(b, int64(m.jid), 16)
+	b = strconv.AppendInt(b, int64(m.jid), 10)
 	b = append(b, '\n')
 	if m.What != 0 {
 		b = append(b, []byte(m.What.String())...)
