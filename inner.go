9cee5b61dbc564bd2bfc7df409ed366a7c921dea
Johan Lindh <johan@linkdata.se>
2023-08-29 13:22:59 +0200

    wip

--- inner.go
+++ inner.go
@@ -4,10 +4,14 @@ import "html/template"
 
 type Inner struct{ Value template.HTML }
 
-func (rq *Request) Inner(param string) (in Inner) {
-	return Inner{Value: template.HTML(param)}
+func (rq *Request) Inner(param string) (vp ValueProxy) {
+	return &Inner{Value: template.HTML(param)}
 }
 
-func (in Inner) JawsGet(e *Element) interface{} {
+func (in *Inner) JawsGet(e *Element) interface{} {
 	return in.Value
+}
+
+func (in *Inner) JawsSet(e *Element, val interface{}) bool {
+	panic("jaws: requires ValueProxy or *atomic.Value instead of Inner")
 }
