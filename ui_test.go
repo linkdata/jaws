cc5758650fa70719deec41cccc96e6222e9bfe20
Johan Lindh <johan@linkdata.se>
2023-10-10 11:46:49 +0200

    wip

--- ui_test.go
+++ ui_test.go
@@ -8,15 +8,25 @@ import (
 	"github.com/matryer/is"
 )
 
+type testStringer struct{}
+
+func (s testStringer) String() string {
+	return "I_Am_A_testStringer"
+}
+
 func TestRequest_JawsRender_DebugOutput(t *testing.T) {
+
 	is := is.New(t)
 	rq := newTestRequest(is)
 	defer rq.Close()
 	rq.Jaws.Debug = true
 	h := string(rq.UI(&testUi{renderFn: func(e *Element, w io.Writer, params []any) {
 		e.Tag(Tag("footag"))
+		e.Tag(e.Request)
+		e.Tag(testStringer{})
 	}}))
 	t.Log(h)
 	is.True(strings.Contains(h, "footag"))
 	is.True(strings.Contains(h, "*jaws.testUi"))
+	is.True(strings.Contains(h, testStringer{}.String()))
 }
