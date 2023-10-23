2ba5958d1b605bc29adb5a439e41c7257b6c3f5f
Johan Lindh <johan@linkdata.se>
2023-10-23 15:40:42 +0200

    go install is a bit broken on OS/X

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 go install golang.org/x/tools/cmd/stringer@latest
-go install github.com/axw/gocov/gocov@latest
-go install github.com/AlekSi/gocov-xml@latest
 go generate ./...
-gocov test -race -failfast -coverprofile=coverage.txt $* ./... | gocov-xml > coverage.xml
+go run github.com/axw/gocov/gocov@latest test -race -failfast -coverprofile=coverage.txt $* ./... | go run github.com/AlekSi/gocov-xml@latest > coverage.xml
 go mod tidy
