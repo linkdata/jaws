6cf26a7da31976509aaedc39b7c01f22052b22e2
Johan Lindh <johan@linkdata.se>
2023-10-25 08:36:46 +0200

    make sure cookie name is legal

--- defaultcookiename.go
+++ defaultcookiename.go
@@ -13,9 +13,15 @@ var DefaultCookieName string
 func init() {
 	DefaultCookieName = "jaws"
 	if s, err := os.Executable(); err == nil {
-		s = strings.Trim(filepath.Base(s), "/.\\")
+		s = filepath.Base(s)
 		s = strings.TrimSuffix(s, filepath.Ext(s))
-		if s != "" {
+		var b []byte
+		for _, ch := range s {
+			if ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9') {
+				b = append(b, byte(ch))
+			}
+		}
+		if len(b) > 0 {
 			DefaultCookieName = s
 		}
 	}
