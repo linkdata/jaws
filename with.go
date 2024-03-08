c14566212331d3819b0d66a6dccfa49641fb8f09
Johan Lindh <johan@linkdata.se>
2024-03-08 13:24:43 +0100

    add comments

--- with.go
+++ with.go
@@ -4,9 +4,11 @@ import (
 	"html/template"
 )
 
+// With is passed as the data parameter when using RequestWriter.Template(),
+// populated with all members set.
 type With struct {
-	*Element
-	RequestWriter
-	Dot   any
-	Attrs template.HTMLAttr
+	*Element                        // the Element being rendered using a template.
+	RequestWriter                   // the RequestWriter
+	Dot           any               // user data parameter
+	Attrs         template.HTMLAttr // HTML attributes string
 }
