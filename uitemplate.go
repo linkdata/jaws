154773bf323b7c286ab86470947855df926e59db
Johan Lindh <johan@linkdata.se>
2024-04-10 10:16:47 +0200

    clean up doc

--- uitemplate.go
+++ uitemplate.go
@@ -13,9 +13,6 @@ func NewUiTemplate(t Template) UiTemplate {
 //
 // The templ argument can either be a string, in which case Jaws.Template.Lookup() will
 // be used to resolve it. Or it can be a *template.Template directly.
-//
-// Note that templates are only rendered once; adding UI tags to a template
-// have no effect, since JawsUpdate is a no-op for them.
 func (rq RequestWriter) Template(templ, dot any, params ...any) error {
 	return rq.UI(NewUiTemplate(rq.rq.MakeTemplate(templ, dot)), params...)
 }
