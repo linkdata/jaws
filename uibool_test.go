dc528e538c76f5ef8048cf8ae0bcd5c0e33a43ea
Johan Lindh <johan@linkdata.se>
2024-04-26 16:48:40 +0200

    simplify

--- uibool_test.go
+++ uibool_test.go
@@ -5,11 +5,12 @@ import (
 	"testing"
 )
 
-func TestUiBool_Locker(t *testing.T) {
+func TestUiBool(t *testing.T) {
+	var l sync.Mutex
+	var rl sync.RWMutex
 	var val bool
-	var mu sync.Mutex
 
-	ui := UiBool{L: &mu, P: &val}
+	ui := UiBool{L: &l, P: &val}
 
 	if ui.JawsGetBool(nil) {
 		t.Fail()
@@ -19,24 +20,7 @@ func TestUiBool_Locker(t *testing.T) {
 		t.Error(x)
 	}
 
-	if !ui.JawsGetBool(nil) {
-		t.Fail()
-	}
-}
-
-func TestUiBool_RLocker(t *testing.T) {
-	var val bool
-	var mu sync.RWMutex
-
-	ui := UiBool{L: &mu, P: &val}
-
-	if ui.JawsGetBool(nil) {
-		t.Fail()
-	}
-
-	if x := ui.JawsSetBool(nil, true); x != nil {
-		t.Error(x)
-	}
+	ui.L = &rl
 
 	if !ui.JawsGetBool(nil) {
 		t.Fail()
