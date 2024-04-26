2974572156b3dcb7e1b15d90dbf9a7393e364b6d
Johan Lindh <johan@linkdata.se>
2024-04-26 16:58:30 +0200

    add UiFloat

---
+++ uifloat.go
@@ -0,0 +1,33 @@
+package jaws
+
+import (
+	"sync"
+)
+
+var _ FloatSetter = UiFloat{}
+
+// UiFloat implements FloatSetter given a sync.Locker (or RLocker) and a float64 pointer.
+type UiFloat struct {
+	L sync.Locker
+	P *float64
+}
+
+func (ui UiFloat) JawsGetFloat(e *Element) (val float64) {
+	if rl, ok := ui.L.(RLocker); ok {
+		rl.RLock()
+		val = *ui.P
+		rl.RUnlock()
+		return
+	}
+	ui.L.Lock()
+	val = *ui.P
+	ui.L.Unlock()
+	return
+}
+
+func (ui UiFloat) JawsSetFloat(e *Element, val float64) (err error) {
+	ui.L.Lock()
+	*ui.P = val
+	ui.L.Unlock()
+	return
+}
---
+++ uifloat_test.go
@@ -0,0 +1,28 @@
+package jaws
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestUiFloat(t *testing.T) {
+	var l sync.Mutex
+	var rl sync.RWMutex
+	var val float64
+
+	ui := UiFloat{L: &l, P: &val}
+
+	if ui.JawsGetFloat(nil) != 0 {
+		t.Fail()
+	}
+
+	if x := ui.JawsSetFloat(nil, -1); x != nil {
+		t.Error(x)
+	}
+
+	ui.L = &rl
+
+	if ui.JawsGetFloat(nil) != -1 {
+		t.Fail()
+	}
+}
