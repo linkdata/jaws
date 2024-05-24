27e00858b94aa0a6ea08c9b1489e46e6d2847e72
Johan Lindh <johan@linkdata.se>
2024-05-24 12:57:11 +0200

    use NewTemplate

--- template.go
+++ template.go
@@ -56,5 +56,5 @@ func NewTemplate(name string, dot any) Template {
 // The name argument is a string to be resolved to a *template.Template
 // using Jaws.LookupTemplate().
 func (rq RequestWriter) Template(name string, dot any, params ...any) error {
-	return rq.UI(Template{Name: name, Dot: dot}, params...)
+	return rq.UI(NewTemplate(name, dot), params...)
 }
