b34e2eba781d21374ab226aef49ec076518633f3
Johan Lindh <johan@linkdata.se>
2024-06-20 13:13:03 +0200

    docs

--- clickhandler.go
+++ clickhandler.go
@@ -3,6 +3,11 @@ package jaws
 import "github.com/linkdata/jaws/what"
 
 type ClickHandler interface {
+	// JawsClick is called when an Element's HTML element or something within it
+	// is clicked in the browser.
+	//
+	// The name parameter is taken from the first 'name' HTML attribute or HTML
+	// 'button' textContent found when traversing the DOM. It may be empty.
 	JawsClick(e *Element, name string) (err error)
 }
 
