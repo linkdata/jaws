e9dd40e047b195c33e24201672910d0f795652ae
Johan Lindh <johan@linkdata.se>
2024-06-27 08:52:26 +0200

    wip

--- jsstring_test.go
+++ jsstring_test.go
@@ -11,15 +11,15 @@ func TestJsString_JawsRender(t *testing.T) {
 	rq := newTestRequest()
 	defer rq.Close()
 
-	val := "text1"
+	val := "text'1"
 	ts := newTestSetter(val)
 	th.NoErr(rq.JsString(ts, "varname"))
-	wantHtml := "<div id=\"Jvar.varname\" data-json='\"text1\"' data-jid=\"Jid.1\"></div>"
+	wantHtml := "<div id=\"Jvar.varname\" data-json='\"text\\u00271\"' data-jid=\"Jid.1\"></div>"
 	if gotHtml := rq.BodyString(); gotHtml != wantHtml {
 		t.Errorf("Request.JsString() = %q, want %q", gotHtml, wantHtml)
 	}
 
-	ts.Set("text2")
+	ts.Set("text'2")
 	rq.Dirty(ts)
 
 	select {
@@ -27,7 +27,7 @@ func TestJsString_JawsRender(t *testing.T) {
 		th.Timeout()
 	case msg := <-rq.outCh:
 		s := msg.Format()
-		if s != "Set\t\t\"varname\\n\\\"text2\\\"\"\n" {
+		if s != "Set\t\t\"varname\\n\\\"text'2\\\"\"\n" {
 			t.Error(strconv.Quote(s))
 		}
 	}
