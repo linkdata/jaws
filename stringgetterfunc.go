038bfb46afd09a99946e2fc169f40481646105d4
Johan Lindh <johan@linkdata.se>
2024-12-18 07:25:23 +0100

    add StringGetterFunc

---
+++ stringgetterfunc.go
@@ -0,0 +1,19 @@
+package jaws
+
+type stringGetterFunc struct {
+	fn   func(*Element) string
+	tags []any
+}
+
+func (g *stringGetterFunc) JawsGet(e *Element) string {
+	return g.fn(e)
+}
+
+func (g *stringGetterFunc) JawsGetTag(e *Request) any {
+	return g.tags
+}
+
+// StringGetterFunc wraps a function and returns a Getter[string]
+func StringGetterFunc(fn func(elem *Element) (s string), tags ...any) Getter[string] {
+	return &stringGetterFunc{fn: fn, tags: tags}
+}
