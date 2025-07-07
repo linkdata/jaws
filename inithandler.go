ecbc84d18a5899ada923b08b2dbe4e052953d892
Johan Lindh <johan@linkdata.se>
2025-07-07 10:46:44 +0200

    wip

--- inithandler.go
+++ inithandler.go
@@ -1,6 +1,6 @@
 package jaws
 
-// InitHandler allows initializing UI getters before their use.
+// InitHandler allows initializing UI getters and setters before their use.
 //
 // You can of course initialize them in the call from the template engine,
 // but at that point you don't have access to the Element or the Element.Context.
