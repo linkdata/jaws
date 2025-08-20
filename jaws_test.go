f04fb6d00ced5758d0ba9f55e4ac64b1523401b0
Johan Lindh <johan@linkdata.se>
2025-08-20 09:53:16 +0200

    move test

--- jaws_test.go
+++ jaws_test.go
@@ -521,3 +521,28 @@ func TestJaws_TemplateLookuper(t *testing.T) {
 	rq.Jaws.RemoveTemplateLookuper(rq.jw.testtmpl)
 	th.Equal(rq.Jaws.LookupTemplate("testtemplate"), nil)
 }
+
+func TestJaws_JsCall(t *testing.T) {
+	th := newTestHelper(t)
+	nextJid = 0
+	rq := newTestRequest()
+	defer rq.Close()
+
+	tss := &testUi{}
+
+	var sb strings.Builder
+	elem := rq.NewElement(tss)
+	th.Equal(elem.Jid(), Jid(1))
+	err := elem.JawsRender(&sb, nil)
+	th.NoErr(err)
+
+	elem.Jaws.JsCall(tss, "somefn", "1.3")
+
+	select {
+	case <-th.C:
+		th.Timeout()
+	case msg := <-rq.outCh:
+		got := msg.Format()
+		th.Equal(got, "Call\tJid.1\tsomefn=1.3\n")
+	}
+}
