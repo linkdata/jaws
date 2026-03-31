756a2bcc1dbfe7c301bbafd582ffaac814319b17
Johan Lindh <johan@linkdata.se>
2026-03-31 14:23:22 +0200

    wip

--- core/request.go
+++ core/request.go
@@ -18,6 +18,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/coder/websocket"
 	"github.com/linkdata/deadlock"
 	"github.com/linkdata/jaws/core/assets"
 	"github.com/linkdata/jaws/core/tags"
@@ -928,4 +929,55 @@ func (rq *Request) MustLog(err error) {
 		jw = rq.Jaws
 	}
 	jw.MustLog(err)
+}
+
+func (rq *Request) startServe() (ok bool) {
+	return rq.claimed.Load() && rq.running.CompareAndSwap(false, true)
+}
+
+func (rq *Request) stopServe() {
+	rq.cancel(nil)
+	rq.Jaws.recycle(rq)
+}
+
+var headerContentTypeJavaScript = []string{"text/javascript"}
+
+// ServeHTTP implements http.HanderFunc.
+//
+// Requires UseRequest() have been successfully called for the Request.
+func (rq *Request) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if rq.startServe() {
+		defer rq.stopServe()
+		if strings.HasSuffix(r.URL.Path, "/noscript") {
+			w.WriteHeader(http.StatusNoContent)
+			rq.cancel(ErrJavascriptDisabled)
+			return
+		}
+		var err error
+		if r.Header.Get("Sec-WebSocket-Key") != "" {
+			if err = rq.validateWebSocketOrigin(r); err != nil {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				rq.cancel(err)
+				return
+			}
+		}
+		var ws *websocket.Conn
+		ws, err = websocket.Accept(w, r, nil)
+		if err == nil {
+			if err = rq.onConnect(); err == nil {
+				incomingMsgCh := make(chan wire.WsMsg)
+				broadcastMsgCh := rq.Jaws.subscribe(rq, 4+len(rq.elems)*4)
+				outboundMsgCh := make(chan wire.WsMsg, cap(broadcastMsgCh))
+				go wire.ReadLoop(rq.ctx, rq.cancelFn, rq.Jaws.Done(), incomingMsgCh, ws)  // closes incomingMsgCh
+				go wire.WriteLoop(rq.ctx, rq.cancelFn, rq.Jaws.Done(), outboundMsgCh, ws) // calls ws.Close()
+				rq.process(broadcastMsgCh, incomingMsgCh, outboundMsgCh)                  // unsubscribes broadcastMsgCh, closes outboundMsgCh
+			} else {
+				defer ws.Close(websocket.StatusNormalClosure, err.Error())
+				var msg wire.WsMsg
+				msg.FillAlert(rq.Jaws.Log(err))
+				_ = ws.Write(r.Context(), websocket.MessageText, msg.Append(nil))
+			}
+		}
+		rq.cancel(err)
+	}
 }
