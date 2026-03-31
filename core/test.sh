192e2ff38e6b13ede075e094e8cc43c7b88de369
Johan Lindh <johan@linkdata.se>
2026-03-31 11:09:06 +0200

    delete unused

--- core/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-go install golang.org/x/tools/cmd/stringer@latest
-go generate ./...
-go run github.com/axw/gocov/gocov@latest test -race -failfast -coverprofile=coverage.txt $* ./... | go run github.com/AlekSi/gocov-xml@latest > coverage.xml
-go mod tidy
