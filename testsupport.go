32aaac535d8e91924fc4642bdfd8f79871cdd924
Johan Lindh <johan@linkdata.se>
2026-04-01 10:44:37 +0200

    wip

--- testsupport.go
+++ testsupport.go
@@ -92,3 +92,33 @@ func NewTestRequest(jw *Jaws, hr *http.Request) (tr *TestRequest) {
 	}
 	return
 }
+
+// SubscribeForTest subscribes rq to broadcasts with the given channel size.
+//
+// It is intended for use by test helpers.
+func (jw *Jaws) SubscribeForTest(rq *Request, size int) chan wire.Message {
+	return jw.subscribe(rq, size)
+}
+
+// PumpSubscriptionsForTest pushes empty subscriptions through the internal queue.
+//
+// It is intended for use by test helpers.
+func (jw *Jaws) PumpSubscriptionsForTest() {
+	for i := 0; i <= cap(jw.subCh); i++ {
+		jw.subCh <- subscription{}
+	}
+}
+
+// ProcessForTest runs the request processing loop with explicit channels.
+//
+// It is intended for use by test helpers.
+func (rq *Request) ProcessForTest(broadcastMsgCh chan wire.Message, incomingMsgCh <-chan wire.WsMsg, outboundMsgCh chan<- wire.WsMsg) {
+	rq.process(broadcastMsgCh, incomingMsgCh, outboundMsgCh)
+}
+
+// RecycleForTest recycles rq back into the request pool.
+//
+// It is intended for use by test helpers.
+func (jw *Jaws) RecycleForTest(rq *Request) {
+	jw.recycle(rq)
+}
