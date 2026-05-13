42f9cb0ca85601cacdadb0bdd906634cce15550f
Johan Lindh <johan@linkdata.se>
2026-05-13 14:21:19 +0200

    refactor(htmlio): optimize HTML tag writing with stack-allocated buffer

--- lib/htmlio/writehtml.go
+++ lib/htmlio/writehtml.go
@@ -27,6 +27,8 @@ var singletonTags = map[string]struct{}{
 	"wbr":     {},
 }
 
+const htmlBufferSize = 128
+
 func needClosingTag(tag string) bool {
 	_, ok := singletonTags[tag]
 	return !ok
@@ -42,10 +44,8 @@ func appendAttrs(b []byte, attrs []template.HTMLAttr) []byte {
 	return b
 }
 
-// WriteHTMLTag writes an HTML start tag with optional id, type, value and raw
-// attribute fragments.
-func WriteHTMLTag(w io.Writer, jid jid.Jid, htmlTag, typeAttr, valueAttr string, attrs []template.HTMLAttr) (err error) {
-	b := jid.AppendStartTagAttr(nil, htmlTag)
+func appendHTMLTag(b []byte, jid jid.Jid, htmlTag, typeAttr, valueAttr string, attrs []template.HTMLAttr) []byte {
+	b = jid.AppendStartTagAttr(b, htmlTag)
 	if typeAttr != "" {
 		b = append(b, ` type=`...)
 		b = strconv.AppendQuote(b, typeAttr)
@@ -56,6 +56,14 @@ func WriteHTMLTag(w io.Writer, jid jid.Jid, htmlTag, typeAttr, valueAttr string,
 	}
 	b = appendAttrs(b, attrs)
 	b = append(b, '>')
+	return b
+}
+
+// WriteHTMLTag writes an HTML start tag with optional id, type, value and raw
+// attribute fragments.
+func WriteHTMLTag(w io.Writer, jid jid.Jid, htmlTag, typeAttr, valueAttr string, attrs []template.HTMLAttr) (err error) {
+	var buf [htmlBufferSize]byte
+	b := appendHTMLTag(buf[:0], jid, htmlTag, typeAttr, valueAttr, attrs)
 	_, err = w.Write(b)
 	return
 }
@@ -71,15 +79,14 @@ func WriteHTMLInput(w io.Writer, jid jid.Jid, typeAttr, valueAttr string, attrs 
 // Singleton tags such as img and input are written without closing tags unless
 // innerHTML is non-empty.
 func WriteHTMLInner(w io.Writer, jid jid.Jid, htmlTag, typeAttr string, innerHTML template.HTML, attrs ...template.HTMLAttr) (err error) {
-	if err = WriteHTMLTag(w, jid, htmlTag, typeAttr, "", attrs); err == nil {
-		if innerHTML != "" || needClosingTag(htmlTag) {
-			var b []byte
-			b = append(b, innerHTML...)
-			b = append(b, "</"...)
-			b = append(b, htmlTag...)
-			b = append(b, '>')
-			_, err = w.Write(b)
-		}
+	var buf [htmlBufferSize]byte
+	b := appendHTMLTag(buf[:0], jid, htmlTag, typeAttr, "", attrs)
+	if innerHTML != "" || needClosingTag(htmlTag) {
+		b = append(b, innerHTML...)
+		b = append(b, "</"...)
+		b = append(b, htmlTag...)
+		b = append(b, '>')
 	}
+	_, err = w.Write(b)
 	return
 }
