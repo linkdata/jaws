d893c8dae6e36c8cc115c63e1140ee683645dcc8
Johan Lindh <johan@linkdata.se>
2026-08-14 14:36:55 +0200

    docs
    Signed-off-by: Johan Lindh <johan@linkdata.se>

--- lib/htmlio/doc.go
+++ lib/htmlio/doc.go
@@ -4,4 +4,8 @@
 // and template.HTMLAttr fragments are trusted and written as-is; callers must
 // not derive them from untrusted data. Use [Attr] or [AppendAttr] to build an
 // attribute whose value is escaped.
+//
+// Tag and attribute names must conform to HTML. Browser parser error recovery
+// or DOM construction of a non-conforming name does not make it a supported
+// input.
 package htmlio
